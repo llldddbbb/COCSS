9c883b9b342a7f4e0ec8f93cb75b8388c74a3852
llldddbbb <571002217@qq.com>
2017-05-28 17:31:03 +0800

    框架整合

--- src/main/java/com/scnu/CocssApplication.java
+++ src/main/java/com/scnu/CocssApplication.java
@@ -2,11 +2,19 @@ package com.scnu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class CocssApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CocssApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String showIndex(){
+		return "index";
 	}
 }
