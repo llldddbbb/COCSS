29513b555934572596f4a885278270661f1178d6
llldddbbb <571002217@qq.com>
2017-05-28 17:19:50 +0800

    框架初始化

---
+++ src/main/java/com/scnu/CocssApplication.java
@@ -0,0 +1,12 @@
+package com.scnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocssApplication.class, args);
+	}
+}
